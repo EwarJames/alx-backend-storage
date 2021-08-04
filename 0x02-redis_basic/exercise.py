411c24245e327c1d2a3ec3dad41399c7bd21a4df
EwarJames <ewaarjames@gmail.com>
2021-08-04 19:13:30 +0300

    Fix Errors

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,21 +2,36 @@
 """"Python Module for redis database"""
 import redis
 from uuid import uuid4
-from typing import Union, Collable, Optional
+from typing import Union, Callable, Optional
 from sys import byteorder
 from functools import wraps
 
 
-def count_calls(method: Collable) -> Collable:
+def replay(method: Callable):
+    """Does the replay"""
+    key = method.__qualname__
+    i = "".join([key,":input"])
+    o = "".join([key, ":input"])
+    count = method.__self__.get(key)
+    i_list = method.__self__._redis.lrange(i, 0, -1)
+    o_list = method.__self__._redis.lrange(o, 0, -1)
+    queue = list(zip(i_list, o_list))
+    print(f"{key} was called {decode_utf8(count)} times:"
+    for k, v in queue:
+          k = decode_utf8(k)
+          v = decode_utf8(v)
+          print(f"{key}({*k})-> {v}")
+
+def count_calls(method: Callable) -> Collable:
     """Counts number of calls to a class method"""
     key = method.__qualname__
 
-    @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        """wraps method"""
-        self._redis.incr(key)
-        return method(self, *args, **kwargs)
-    return wrapper
+@wraps(method)
+def wrapper(self, *args, **kwargs):
+    """wraps method"""
+    self._redis.incr(key)
+    return method(self, *args, **kwargs)
+return wrapper
 
 
 class Cache:
@@ -33,7 +48,7 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Collable] = None)\
+    def get(self, key: str, fn: Optional[Callable] = None)\
             -> Union[str, bytes, int, float]:
         """"Return data converted to desired format"""
         data = self._redis.get(key)
