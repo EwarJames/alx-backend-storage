a064d314c6a8890c61f83fa8d1868645a95cbabb
EwarJames <ewaarjames@gmail.com>
2021-08-05 05:31:34 +0300

    Get webpage

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 """Web module"""
 import requests
+#import redis
+#from typing import Callable
+#from functools import wraps
 
 
-def get_page():
+#r = redis.Redis()
+
+def get_page(url: str) -> str:
     """Gets the page"""
-    return
+    req = requests.get(url)
+    return req.text
