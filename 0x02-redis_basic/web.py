67dbd06f21c1d0d8d0b12ad16dd3217ec82b825a
EwarJames <ewaarjames@gmail.com>
2021-08-05 05:43:01 +0300

    Get web page

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python3
 """Web module"""
 import requests
-#import redis
-#from typing import Callable
-#from functools import wraps
+import redis
+from typing import Callable
+from functools import wraps
 
 
-#r = redis.Redis()
+r = redis.Redis()
+
+
+def count_calls(method: Callable) -> Collable:
+    """Defines number of calls"""
+
+    @wraps(method)
+    def wrapper(url):
+        """"wrapper decorator"""
+        r.incr(f"count:{url}")
+        cached_html = r.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode("utf-8")
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+    return wrapper
+
 
 def get_page(url: str) -> str:
     """Gets the page"""
