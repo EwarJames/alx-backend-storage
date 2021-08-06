49e96db4d3203b6cb6d0dd1898e62c68be8c6c81
EwarJames <ewaarjames@gmail.com>
2021-08-06 17:13:42 +0300

    Lists school by topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module to list specific topics"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """"Returns lists of schools with specific topics"""
+    return mongo_collection.find("topics": topic)
