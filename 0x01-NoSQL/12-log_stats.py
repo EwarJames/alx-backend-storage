199b02e88df3ad065fa892bd28ae118b44f1b519
EwarJames <ewaarjames@gmail.com>
2021-08-06 17:57:38 +0300

    Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 def log_stats():
     """"Provides some stats about Ngnix logs stored in Mongodb"""
-    client = MongoClient("https://127.0.0.1:27017")
+    client = MongoClient("mongodb://127.0.0.1:27017")
     logs_list = client.logs.nginx
     all_logs = logs_list.count_documents({})
     get = logs_list.count_documents({"method": "GET"})
@@ -25,4 +25,4 @@ def log_stats():
     print(f"{path} status check")
 
 if __name__ == "__main__":
-        log_stats()
+    log_stats()
