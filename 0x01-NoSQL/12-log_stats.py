c52e932c3c5f0ce43b012fafb4e46a56b1f59611
EwarJames <ewaarjames@gmail.com>
2021-08-06 19:42:10 +0300

    Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,19 +3,17 @@
 from pymongo import MongoClient
 
 
-client = MongoClient()
-stat = client.logs.nginx
-
-all_logs = stat.count_documents({})
-get = stat.count_documents({"method": "GET"})
-post = stat.count_documents({"method": "POST"})
-delete = stat.count_documents({"method": "DELETE"})
-put = stat.count_documents({"method": "PUT"})
-patch = stat.count_documents({"method": "PATCH"})
-path = stat.count_documents({"method": "GET", "path": "/status"})
-
-
-if __name__ == "__main__":
+def log_stats():
+    """Log stats in mongodb"""
+    client = MongoClient('mongodb://127:0.0.1:27017')
+    stat = client.logs.nginx
+    all_logs = stat.count_documents({})
+    get = stat.count_documents({"method": "GET"})
+    post = stat.count_documents({"method": "POST"})
+    put = stat.count_documents({"method": "PUT"})
+    patch = stat.count_documents({"method": "PATCH"})
+    delete = stat.count_documents({"method": "DELETE"})
+    path = stat.count_documents({"method": "GET", "path": "/status"})
     print(f"{all_logs} logs")
     print("Methods")
     print(f"\tmethod GET: {get}")
@@ -24,3 +22,7 @@ if __name__ == "__main__":
     print(f"\tmethod PATCH: {patch}")
     print(f"\tmethod DELETE: {delete}")
     print(f"{path} status check")
+
+
+if __name__ == "__main__":
+    log_stats()
