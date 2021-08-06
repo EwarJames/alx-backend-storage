cc70a1875596c6498051b4a5b189070a7686af0c
EwarJames <ewaarjames@gmail.com>
2021-08-06 16:38:22 +0300

    Insert a document in a collection based on kwargs

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Insert a new document in python using pymongo"""
+
+
+def insert_school(mongo_colection, **kwargs):
+    """Inserts a new document in a collection based on kwargs"""
+    docs = mongo_collection.insert_one(kwargs)
+    return docs.inserted_id
